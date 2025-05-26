e5a1856f26dc82856e0ab4aabc47fedac23f95c4
lucasfelz <lucasfelz@gmail.com>
2025-05-26 02:19:41 -0300

    atualização 1.0

--- index.py
+++ index.py
@@ -1,30 +1,18 @@
-class Hero:
-    def __init__(self, name):
-        self.name = name
-        self.experience = 0
-        self.number_of_kill_monsters = 0
-
-    def kill_monsters(self):
-        self.experience += 10
-        self.number_of_kill_monsters += 1
-        print(f"{self.name} matou um monstro. XP atual: {self.experience}")
-
-    def exibir_info_heroi(self):
-        print(f"Heroi: {self.name}")
-        print(f"Experiência: {self.experience}")
-        print(f"Monstros derrotados: {self.number_of_kill_monsters}")
-
-def main():
-    hero1 = Hero('Lucas Felz')
-    
-    while hero1.experience < 5001:
-        hero1.kill_monsters()
 
 
-    print(f"{hero1.name} alcançou o ranking ouro ultrapassando 5001 de XP alcançada!\n")
-    hero1.exibir_info_heroi()
 
-    
+def main(name, experience, number_of_kill_monsters):
+    experience += 10
+    number_of_kill_monsters += 1
+    print(f"{name} matou um monstro. XP atual: {experience}")
+    return experience, number_of_kill_monsters
 
-if __name__ == '__main__':
-    main()
+name = 'Lucas Felz'
+experience = 0
+number_of_kill_monsters = 0
+
+while experience < 5001:
+    experience, number_of_kill_monsters = main(name, experience, number_of_kill_monsters)
+
+
+print(f"{name} alcançou o ranking ouro ultrapassando 5001 de XP alcançada!\n")
